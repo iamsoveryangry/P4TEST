3e7a4e268a4d6ce46ae9f1f3d0edf89d6bbb37c1
Dorothy <parkhye@myvuw.ac.nz>
2019-10-06 22:15:54 +1300

    testttt

---
+++ randomizer.js
@@ -0,0 +1,35 @@
+var randomizer = function(){
+   
+		    var links=[
+		    'https://uroclub.com//',//uroclub
+		    'https://www.youtube.com/watch?v=E9_amg-Aos4',//hawaii chair
+		    'https://www.slapchop.com',//slap chop
+		    'https://www.shamwow.com/SHMWOW/6.0000/Index.dtm?otsid=9912',//shamwow
+		    'https://www.youtube.com/watch?v=Cp5FAbJvUEY"',//potty putter
+		    'https://www.youtube.com/watch?v=Zw2uYZ9_0uk',//thighmaster
+		    'https://www.youtube.com/watch?v=BtFY_4lwmtM',//bedazzler
+		    'https://www.youtube.com/watch?v=Lmy9R_WtPbg',//aspray
+		    'https://www.youtube.com/watch?v=fDVVUfm2pmo',//oddysey phone sex
+		    'https://www.youtube.com/watch?v=H5pRqsAaljM',//soloflex
+		    'https://www.youtube.com/watch?v=WasVVm8dgvc',//GLH
+		    'https://www.youtube.com/watch?v=vYm1qIRnqQk',//CATCH IT
+		    'https://www.youtube.com/watch?v=H8B59wNdqKQ',//The Flo
+		    'https://www.youtube.com/watch?v=ZjfB4qDx3RM',//Stop the Insanity
+		    'https://www.youtube.com/watch?v=2wuB_ufWhi8',//BluBlockers
+		    'https://www.youtube.com/watch?v=dJoo7Tgjr8U',//Punk CD
+		    'https://www.youtube.com/watch?v=W8wgZUKcPmM',//Encyclopedia Britannica
+		    'https://www.youtube.com/watch?v=gmybIIAhxGI',//Understanding Computers 1
+		    'https://www.youtube.com/watch?v=1Li6vklePQM',//No Wet Wonder Foam
+		    'https://www.youtube.com/watch?v=MBYXpC0dtHU',//Understanding Computers 2
+		    'https://www.youtube.com/watch?v=hqIrzkPDtrk',//Mr. Microphone
+		    'https://www.youtube.com/watch?v=gw1g2yKxb0I'//Tiddy Bear
+		    ];
+		 
+		    var max = (links.length)
+
+ window.open(links[Math.floor(Math.random()*max)]);
+
+  var link = links[randomNumber];
+  window.location = "http://" + link;
+
+};
